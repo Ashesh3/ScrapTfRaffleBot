774432efd69db80a8213c655f2fa4dca2b481c50
Ashesh <>
2018-02-22 14:34:20 +0530

    v1

--- bot.php
+++ bot.php
@@ -5,14 +5,15 @@
 Usage:
 1. Host in any webserver
 2. Make list.txt in the same directory with the content "XO12," (without quotes but with that comma)
-3. Set a cron job or manually visit this script after some time
-4. If it throws an error, just to go scrap.tf and enter any raffle manually and continue using it.
-5. Logger
+3. Set cookies in script [or maybe csrf token (maybe)]
+4. Set a cron job or manually visit this script after some time
+5. If it throws an error, just to go scrap.tf and enter any raffle manually and continue using it.
 
 -Features:
 + Instant Entry
 + Anti Ban
 + Easy to use
++ Logger
 
 
 Enjoy and **** Jesse XD
